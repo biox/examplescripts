6d072adb3c4ef622e4e1d3a24f0d9b0a05c982f1
Jesse Olson <jessebolson@icloud.com>
2017-06-14 20:22:24 -0500

    Remove unecessary int conversion

--- file-reader.rb
+++ file-reader.rb
@@ -7,7 +7,7 @@ array = Array.new
 File.open(filename) do |f|
   f.each_line.lazy.each do |line|
     array << line
-    if array.size.to_i > selection
+    if array.size > selection
       array.sort.drop(1)
     end
   end
