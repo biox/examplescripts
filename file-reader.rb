482d885d524918d2ba7ada1e92bf09bd8eb0e135
Jesse Olson <jessebolson@icloud.com>
2017-06-14 20:20:37 -0500

    Fix int problem

--- file-reader.rb
+++ file-reader.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 
 filename = ARGV[0]
-selection = ARGV[1]
+selection = ARGV[1].to_i
 array = Array.new
 
 File.open(filename) do |f|
   f.each_line.lazy.each do |line|
     array << line
-    if array.size > selection
+    if array.size.to_i > selection
       array.sort.drop(1)
     end
   end
